2f4d34d6b83b904eab078162f202fab923e8132a
cycIes <alexisvu77@gmail.com>
2025-07-01 02:04:58 -0500

    pass 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let removed = [];
+    for (item of array) {
+        if (remove.includes(item)) {
+            continue;
+        }
+        removed.push(item);
+    }
+    return removed;
 };
 
 // Do not edit below this line
