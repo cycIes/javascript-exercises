0c19b12e56496f79541676744093be0a829b5e93
cycIes <alexisvu77@gmail.com>
2025-07-01 02:31:03 -0500

    pass 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } 
+
+    if (Math.floor(num1) !== num1 || Math.floor(num2) !== num2) {
+        return 'ERROR';
+    }
+
+    if (typeof num1 !== 'number' || typeof num1 !== 'number') {
+        return 'ERROR';
+    }
+
+    if (num1 <= num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = num1; i >= num2; i--) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
