627a5d7397abd781c74d6cc445fe1ab79a2b2735
cycIes <alexisvu77@gmail.com>
2025-06-30 20:29:37 -0500

    pass 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    
+    let repeated = '';
+    for (let i = 0; i < times; i++) {
+        repeated += word;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
