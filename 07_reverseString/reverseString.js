0baa4bf7da2a507e75f34d50fd12ff8af8292836
cycIes <alexisvu77@gmail.com>
2025-06-30 20:40:50 -0500

    pass 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    stringArray.reverse();
+    const reverse = stringArray.join('');
+    return reverse;
 };
 
 // Do not edit below this line
