99b32cd8c3c41d02323845387a7a153d9bee697e
cycIes <79021718+cycIes@users.noreply.github.com>
2025-07-29 23:03:58 +0200

    pass 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
 
+    if (n === 0) {
+        return 0;
+    }
+
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    let first = 0;
+    let second = 1;
+    for (let i = 1; i < n; i++) {
+        let temp = second;
+        second += first;
+        first = temp;
+    }
+    return second;
 };
 
 // Do not edit below this line
