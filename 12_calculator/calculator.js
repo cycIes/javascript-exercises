66badf5ea096cd84a73ae2f77c3572afe1728964
cycIes <79021718+cycIes@users.noreply.github.com>
2025-07-29 19:50:35 +0200

    pass 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((sum, currNum) => sum + currNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product, currNum) => product * currNum, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+
+  for (let i = num; i > 1; i--) {
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
