7376f640ca3e654c941da3a78a4ab7a3b088ccc4
cycIes <79021718+cycIes@users.noreply.github.com>
2025-07-29 23:44:28 +0200

    pass 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let today = new Date();
+    const thisYear = today.getFullYear();
 
+    const oldest = people.reduce((currOld, curr) => {
+            let oldestAge;
+            let currAge;
+            if ("yearOfDeath" in currOld) {
+                oldestAge = currOld.yearOfDeath - currOld.yearOfBirth;
+            } else {
+                oldestAge = thisYear - currOld.yearOfBirth;
+            }
+            if ("yearOfDeath" in curr) {
+                currAge = curr.yearOfDeath - curr.yearOfBirth;
+            } else {
+                currAge = thisYear - curr.yearOfBirth;
+            }
+            if (currAge < oldestAge) {
+                return currOld;
+            }
+            return curr;
+        });
+
+    return oldest;
 };
 
 // Do not edit below this line
